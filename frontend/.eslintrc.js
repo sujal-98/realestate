218d14cd6f134745d951343f73e2e24ea4a14f44
sujal <117587914+sujal-98@users.noreply.github.com>
2025-01-01 19:33:14 +0530

    Delete frontend directory

--- frontend/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "rules": {
-      "no-unused-vars": "off",
-      "jsx-a11y/img-redundant-alt": "off",
-      "jsx-a11y/anchor-is-valid": "off"
-    }
-  };
-  
--- frontend/src/pages/images.jsx
+++
@@ -1,4 +0,0 @@
-const Images=['/images/images1.jpeg',
-'/images/images1.jpeg','/images/images2.jpeg']
-
-module.exports={Images};
